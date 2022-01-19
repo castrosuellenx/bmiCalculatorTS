8064be3659213b8afba54815c9c1c7f133139383
Suellen Castro <castrosuellenx@gmail.com>
2022-01-19 11:25:46 -0300

    add openDrawer on Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import SwitchTheme from '../../components/SwitchTheme';
+import {useTheme} from 'styled-components';
+import {DrawerActions} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
 import * as S from './styles';
+import SwitchTheme from '../../components/SwitchTheme';
+import {MenuDark, MenuLight} from '../../assets';
 
 const Home: React.FC = () => {
+  const theme = useTheme();
+  const navigation = useNavigation();
+
   return (
     <S.Header>
+      <S.MenuButton
+        onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+        {theme.title === 'dark' ? (
+          <MenuDark height={'100%'} width={'100%'} />
+        ) : (
+          <MenuLight height={'100%'} width={'100%'} />
+        )}
+      </S.MenuButton>
+
       <S.Title>BMI Calculator</S.Title>
+
+      <S.BoxToAlign />
     </S.Header>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,10 +4,16 @@ import {RFValue} from 'react-native-responsive-fontsize';
 export const Header = styled.View`
   flex-direction: row;
   width: 95%;
-  justify-content: center;
   background-color: ${({theme}) => theme.colors.boxSecondary};
   border-radius: ${RFValue(27)}px;
   margin-vertical: ${RFValue(5)}px;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const MenuButton = styled.TouchableOpacity`
+  width: ${RFValue(27)}px;
+  height: ${RFValue(27)}px;
 `;
 
 export const Title = styled.Text`
@@ -15,4 +21,9 @@ export const Title = styled.Text`
   font-family: ${({theme}) => theme.fontFamily.bold}
   color: ${({theme}) => theme.colors.text};
   padding-vertical: ${RFValue(30)}px;
+`;
+
+export const BoxToAlign = styled.TouchableOpacity`
+  width: ${RFValue(27)}px;
+  height: ${RFValue(27)}px;
 `;
