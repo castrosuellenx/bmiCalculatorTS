a7f95438366a95b96fabca1d7dc6f70da61e2f22
Suellen Castro <castrosuellenx@gmail.com>
2022-01-18 21:39:24 -0300

    add drawer navigator

--- src/routes/DrawerMenu.tsx
+++ src/routes/DrawerMenu.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {useTheme} from 'styled-components/native';
+import {RFValue} from 'react-native-responsive-fontsize';
 
+import CustomDrawer from '../components/CustomDrawer';
 import Home from '../screens/Home';
 import History from '../screens/History';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerMenu = () => {
+  const theme = useTheme();
+
   return (
     <Drawer.Navigator
       initialRouteName="Home"
       screenOptions={{
         headerShown: false,
-        drawerActiveTintColor: '#080',
-        drawerActiveBackgroundColor: '#EEE',
+        drawerActiveBackgroundColor: theme.colors.primary,
+        drawerInactiveBackgroundColor: theme.colors.boxPrimary,
         drawerLabelStyle: {
-          fontFamily: 'Leelawadee Bold',
-          fontWeight: 'normal',
-          fontSize: 20,
+          fontFamily: theme.fontFamily.bold,
+          color: theme.colors.text,
+          fontSize: RFValue(22),
+          paddingLeft: RFValue(12),
         },
-      }}>
+      }}
+      drawerContent={rest => <CustomDrawer {...rest} />}>
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="History" component={History} />
     </Drawer.Navigator>
