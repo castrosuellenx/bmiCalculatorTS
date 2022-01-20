c6e9f61f1d4ab32e3f48a2d83b0dfe0160550f89
Suellen Castro <castrosuellenx@gmail.com>
2022-01-19 23:42:55 -0300

    add ButtonPrimary

---
+++ src/components/Buttons/ButtonPrimary/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {TouchableOpacityProps} from 'react-native';
+
+import * as S from './styles';
+
+export type Props = {
+  title: string;
+} & TouchableOpacityProps;
+
+const ButtonPrimary: React.FC<Props> = ({title, ...rest}) => {
+  return (
+    <S.Container {...rest}>
+      <S.Title>{title}</S.Title>
+    </S.Container>
+  );
+};
+
+export default ButtonPrimary;
---
+++ src/components/Buttons/ButtonPrimary/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity``;
+
+export const Title = styled.Text`
+  color: #000;
+`;
