e31613616e67763951db637d433506d6f2bfb6c6
Suellen Castro <castrosuellenx@gmail.com>
2022-01-22 12:25:56 -0300

    add Component Height

---
+++ src/components/Height/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Height: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Title>Height</S.Title>
+    </S.Container>
+  );
+};
+
+export default Height;
