df1819f1489364609f8d41ed6edad55e87d56fc5
Suellen Castro <castrosuellenx@gmail.com>
2022-01-22 12:26:13 -0300

    update Home Screen add ButtonPrimary

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useTheme} from 'styled-components/native';
-import Header from '../../components/Header';
-import Gender from '../../components/Gender';
+import {useNavigation} from '@react-navigation/native';
 
 import * as S from './styles';
+import Header from '../../components/Header';
+import Gender from '../../components/Gender';
 import ButtonPrimary from '../../components/Buttons/ButtonPrimary';
 
 const Home: React.FC = () => {
   const theme = useTheme();
+  const navigation = useNavigation();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onCalculate = () => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      navigation.navigate('Result');
+    }, 400);
+  };
 
   return (
     <S.Container>
@@ -20,7 +32,11 @@ const Home: React.FC = () => {
       <S.BoxSecondary>
         <Gender />
 
-        <ButtonPrimary title="Calculate your BMI" />
+        <ButtonPrimary
+          title="Calculate your BMI"
+          loading={isLoading}
+          onPress={onCalculate}
+        />
       </S.BoxSecondary>
     </S.Container>
   );
